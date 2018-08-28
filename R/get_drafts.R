c5a180c0d55629ed409e806bc63acb889b2af439
Evan Oppenheimer <eoppe1022@gmail.com>
2018-08-27 22:32:38 -0700

    Add files via upload

---
+++ R/get_drafts.R
@@ -0,0 +1,113 @@
+#' Gets draft information, players' names, and player URLs for specified draft and season
+#' 
+#' Returns a data frame of draft information, players' names, and player URLs for user supplied drafts (NHL Entry Draft, CHL Import Draft, etc.) & seasons.
+#' 
+#' @param .draft_type The type of draft for which the user wants to scrape data. Draft types must be typed exactly as they are found on EliteProspects (though case doesn't matter). Draft types include -- but are not limited to -- NHL Entry Draft, NHL Expansion Draft, KHL Draft, NWHL Draft, CWHL Draft, CHL Import Draft, and OHL U18 Priority Selection. Others may be found at the bottom of the page at \url{https://www.eliteprospects.com/draft/nhl-entry-draft}.
+#' @param .draft_year Seasons for which the user wants to scrape data. Must be of the form \code{2018}, \code{1996}, etc -- only  a single 4-digit number.
+#' @param .progress Sets a Progress Bar. Defaults to \code{TRUE}.
+#' @param ... Allows the user to supply other information to the function. If you don't know what this means, then don't worry about it.
+#' @examples 
+#' get_drafts("chl import draft", "2018")
+#' 
+#' get_drafts(c("nhl entry draft", "khl draft"), c("2018", "2017", "1994"))
+#' 
+#' @export
+#' @import dplyr
+#' 
+get_drafts <- function(.draft_type, .draft_year, .progress = TRUE, ...) {
+  
+  draft_types <- .draft_type %>% 
+    as_tibble() %>% 
+    purrr::set_names(".draft_type") %>% 
+    mutate(.draft_type = stringr::str_replace_all(.draft_type, " ", "-"))
+  
+  draft_years <- .draft_year %>%
+    as_tibble() %>%
+    purrr::set_names(".draft_year")
+  
+  mydata <- tidyr::crossing(draft_types, draft_years)
+  
+  if (.progress) {
+    
+    pb <- progress::progress_bar$new(format = "get_drafts() [:bar] :percent eta: :eta", clear = FALSE, total = nrow(mydata), show_after = 0) 
+    
+    pb$tick(0)}
+  
+  .get_drafts <- function(.draft_type, .draft_year, ...) {
+    
+    seq(30, 35, by = 0.001) %>%
+      sample(1) %>%
+      Sys.sleep()
+
+    page <- stringr::str_c("https://www.eliteprospects.com/draft/", .draft_type, "/", .draft_year) %>% xml2::read_html()
+    
+    draft_league <- page %>%
+      rvest::html_nodes(".plytitle") %>%
+      rvest::html_text() %>%
+      stringr::str_replace("[0-9]{4,4}", "") %>%
+      stringr::str_squish()
+    
+    draft_year <- .draft_year
+    
+    draft_pick_info <- page %>%
+      rvest::html_nodes("#drafted-players td:nth-child(1)") %>%
+      rvest::html_text() %>%
+      stringr::str_squish() %>%
+      as_tibble() %>%
+      mutate(round = ifelse(stringr::str_detect(value, "ROUND"), value, NA)) %>%
+      tidyr::fill(round) %>%
+      filter(!stringr::str_detect(value, "ROUND")) %>%
+      mutate(value = stringr::str_replace(value, "#", "")) %>%
+      mutate(round = stringr::str_replace(round, "ROUND", "")) %>%
+      mutate_all(stringr::str_squish) %>%
+      rename(round = round, pick_number = value)
+    
+    draft_team <- page %>%
+      rvest::html_nodes(".team a") %>%
+      rvest::html_text() %>%
+      stringr::str_squish() %>%
+      as_tibble() %>%
+      purrr::set_names("draft_team")
+    
+    player_info <- page %>%
+      rvest::html_nodes("#drafted-players .txt-blue a") %>%
+      rvest::html_text() %>%
+      stringr::str_squish() %>%
+      as_tibble() %>%
+      mutate(position = stringr::str_split(value, "\\(", simplify = TRUE, n = 2)[,2]) %>%
+      mutate(position = stringr::str_split(position, "\\)", simplify = TRUE, n = 2)[,1]) %>%
+      mutate(name = stringr::str_split(value, "\\(", simplify = TRUE, n = 2)[,1]) %>%
+      mutate_all(stringr::str_squish)
+    
+    player_url <- page %>%
+      rvest::html_nodes("#drafted-players .txt-blue a") %>%
+      rvest::html_attr("href") %>%
+      as_tibble() %>%
+      purrr::set_names("player_url")
+    
+    player_names_with_no_selection <- page %>%
+      rvest::html_nodes("#drafted-players td.player") %>%
+      rvest::html_text() %>%
+      stringr::str_squish() %>%
+      as_tibble() %>%
+      purrr::set_names("value")
+    
+    all_data <- player_info %>%
+      bind_cols(player_url) %>%
+      right_join(player_names_with_no_selection, by = c("value" = "value")) %>%
+      bind_cols(draft_pick_info) %>%
+      bind_cols(draft_team) %>%
+      mutate(draft_league = draft_league) %>%
+      mutate(draft_year = draft_year) %>%
+      select(-c(value)) %>%
+      select(draft_league, draft_year, pick_number, round, draft_team, name, position, player_url)
+    
+    if (.progress) {pb$tick()}
+    
+    return(all_data)}
+    
+  draft_data <- purrr::map2_dfr(mydata[[".draft_type"]], mydata[[".draft_year"]], elite::persistently(.get_drafts, max_attempts = 10))
+    
+  return(draft_data)
+  
+}
--- R/get_player_stats_individual.R
+++ R/get_player_stats_individual.R
@@ -2,7 +2,7 @@
 #' 
 #' Returns a data frame of players, their bio information (age, birth place, etc.), and career statistics for user supplied player URLs and names. 
 #' 
-#' @param ... Function requires a \code{player_url} and \code{name}. Additional data may be supplied. All of this information comes directly from \code{get_player_stats_team()} and \code{get_teams()}, if desired.
+#' @param ... Function requires a \code{player_url} and \code{name}. Additional data may be supplied. All of this information comes directly from \code{get_player_stats_team()} and \code{get_teams()} or \code{get_drafts()}, if desired.
 #' @param .progress Sets a Progress Bar. Defaults to \code{TRUE}.
 #' @param .strip_redundancy Removes variables \code{name_}, \code{player_url_}, and \code{position_}, as they're the same as \code{name}, \code{player_url}, and \code{position}. Defaults to \code{TRUE}.
 #' @examples 
@@ -11,6 +11,10 @@
 #' teams <- get_teams("ohl", "2017-2018")
 #' stats_team <- get_player_stats_team(teams)
 #' get_player_stats_individual(stats_team)
+#' 
+#' # The function also works in conjunction with get_drafts()
+#' drafts <- get_drafts("nhl entry draft", "2018")
+#' get_player_stats_individual(drafts)
 #' 
 #' # All functions are easily pipeable too
 #' get_teams(c("shl", "allsvenskan"), c("2008-2009", "2009-2010", "2010-2011")) %>%
@@ -43,75 +47,126 @@ get_player_stats_individual <- function(..., .progress = TRUE, .strip_redundancy
   
   .get_player_stats_individual <- function(player_url, name, ...) {
     
-    seq(30, 35, by = 0.001) %>%
-      sample(1) %>%
-      Sys.sleep()
+    if (is.na(player_url)) {
+      
+      all_data <- tibble(shot_handedness = NA, birth_place = NA, birth_country = NA, birthday = NA, height = NA, weight = NA, age = NA, name_ = NA, position_ = NA, player_url_ = NA)
+      
+      player_statistics <- NA %>% 
+        as_tibble() %>% 
+        purrr::set_names("captaincy_") %>% 
+        tidyr::nest()
+      
+      all_data <- all_data %>% 
+        bind_cols(player_statistics) %>% 
+        rename(player_statistics = data)
+      
+    }
     
-    page <- player_url %>% xml2::read_html()
-    
-    vitals <- page %>%
-      rvest::html_nodes('[class="col-xs-8 fac-lbl-dark"]') %>%
-      rvest::html_text() %>%
-      magrittr::extract(1:9) %>%
-      stringr::str_squish() %>%
-      purrr::set_names("birthday", "age", "birth_place", "birth_country", "youth_team", "position_", "height", "weight", "shot_handedness") %>%
-      t() %>%
-      as_tibble() %>%
-      mutate(birthday = lubridate::mdy(birthday, quiet = TRUE)) %>%
-      mutate(height = stringr::str_split(height, '"', simplify = TRUE, n = 2)[,1]) %>%
-      mutate(feet_tall = stringr::str_split(height, "'", simplify = TRUE, n = 2)[,1]) %>%
-      mutate(inches_tall = stringr::str_split(height, "'", simplify = TRUE, n = 2)[,2]) %>%
-      mutate(height = (as.numeric(feet_tall) * 12) + as.numeric(inches_tall)) %>%
-      mutate(weight = stringr::str_split(weight, "lbs", simplify = TRUE, n = 2)[,1]) %>%
-      mutate(name_ = name) %>%
-      mutate(player_url_ = player_url) %>%
-      mutate_all(~stringr::str_trim(., side = "both")) %>%
-      mutate_all(~na_if(., "-")) %>%
-      mutate_all(~na_if(., "")) %>%
-      select(-c(feet_tall, inches_tall, age, youth_team))
-    
-    player_statistics <- page %>%
-      rvest::html_node('[class="table table-striped table-condensed table-sortable player-stats highlight-stats"]') %>%
-      rvest::html_table() %>%
-      purrr::set_names("season_", "team_", "league_", "games_played_", "goals_", "assists_", "points_", "penalty_minutes_", "plus_minus_", "blank_", "playoffs_", "games_played_playoffs_", "goals_playoffs_", "assists_playoffs_", "points_playoffs_", "penalty_minutes_playoffs_", "plus_minus_playoffs_") %>%
-      as_tibble() %>%    
-      mutate_all(~na_if(., "-")) %>%      
-      mutate(captaincy_ = stringr::str_split(team_, "\U201C", simplify = TRUE, n = 2)[,2]) %>%
-      mutate(captaincy_ = stringr::str_split(captaincy_, "\U201D", simplify = TRUE, n = 2)[,1]) %>%
-      mutate(team_ = stringr::str_split(team_, "\U201C", simplify = TRUE, n = 2)[,1]) %>%
-      mutate(season_ = replace(season_, season_ == "", NA)) %>%
-      tidyr::fill(season_) %>%
-      mutate(season_short_ = as.numeric(stringr::str_split(season_, "-", simplify = TRUE, n = 2)[,1]) + 1) %>%
-      mutate(birthday = vitals[["birthday"]]) %>%
-      mutate(draft_eligibility_date_ = stringr::str_c(as.character(season_short_), "09-15", sep = "-")) %>%
-      mutate(age_ = elite::get_years_difference(birthday, draft_eligibility_date_)) %>%
-      mutate_all(stringr::str_squish) %>%
-      mutate_all(as.character) %>%      
-      mutate_all(~na_if(., "")) %>%
-      select(-c(blank_, playoffs_, draft_eligibility_date_, birthday)) %>%
-      select(team_, league_, captaincy_, season_, season_short_, age_, games_played_, goals_, assists_, points_, penalty_minutes_, plus_minus_, games_played_playoffs_, goals_playoffs_, assists_playoffs_, points_playoffs_, penalty_minutes_playoffs_, plus_minus_playoffs_) %>% 
-      mutate_at(vars(c(team_, league_, captaincy_, season_)), as.character) %>%
-      mutate_at(vars(-c(team_, league_, captaincy_, season_)), as.numeric) %>%
-      tidyr::nest()
-    
-    all_data <- vitals %>% 
-      bind_cols(player_statistics) %>% 
-      rename(player_statistics = data)
+    else {
+      
+      seq(30, 35, by = 0.001) %>%
+        sample(1) %>%
+        Sys.sleep()
+      
+      page <- player_url %>% xml2::read_html()
+      
+      vitals <- page %>%
+        rvest::html_nodes('[class="col-xs-8 fac-lbl-dark"]') %>%
+        rvest::html_text() %>%
+        magrittr::extract(1:9) %>%
+        stringr::str_squish() %>%
+        purrr::set_names("birthday", "age", "birth_place", "birth_country", "youth_team", "position_", "height", "weight", "shot_handedness") %>%
+        t() %>%
+        as_tibble() %>%
+        mutate(birthday = lubridate::mdy(birthday, quiet = TRUE)) %>%
+        mutate(height = stringr::str_split(height, '"', simplify = TRUE, n = 2)[,1]) %>%
+        mutate(feet_tall = stringr::str_split(height, "'", simplify = TRUE, n = 2)[,1]) %>%
+        mutate(inches_tall = stringr::str_split(height, "'", simplify = TRUE, n = 2)[,2]) %>%
+        mutate(height = (as.numeric(feet_tall) * 12) + as.numeric(inches_tall)) %>%
+        mutate(weight = stringr::str_split(weight, "lbs", simplify = TRUE, n = 2)[,1]) %>%
+        mutate(name_ = name) %>%
+        mutate(player_url_ = player_url) %>%
+        mutate_all(~stringr::str_trim(., side = "both")) %>%
+        mutate_all(~na_if(., "-")) %>%
+        mutate_all(~na_if(., "")) %>%
+        select(-c(feet_tall, inches_tall, age, youth_team))
+      
+      if (stringr::str_detect(vitals[["position_"]], "G")) {
+        
+        player_statistics <- NA %>% 
+          as_tibble() %>% 
+          purrr::set_names("captaincy_") %>% 
+          tidyr::nest()
+        
+      }
+      
+      else {
+      
+      player_statistics <- page %>%
+        rvest::html_node('[class="table table-striped table-condensed table-sortable player-stats highlight-stats"]') %>%
+        rvest::html_table() %>%
+        purrr::set_names("season_", "team_", "league_", "games_played_", "goals_", "assists_", "points_", "penalty_minutes_", "plus_minus_", "blank_", "playoffs_", "games_played_playoffs_", "goals_playoffs_", "assists_playoffs_", "points_playoffs_", "penalty_minutes_playoffs_", "plus_minus_playoffs_") %>%
+        as_tibble() %>%    
+        mutate_all(~na_if(., "-")) %>%      
+        mutate(captaincy_ = stringr::str_split(team_, "\U201C", simplify = TRUE, n = 2)[,2]) %>%
+        mutate(captaincy_ = stringr::str_split(captaincy_, "\U201D", simplify = TRUE, n = 2)[,1]) %>%
+        mutate(team_ = stringr::str_split(team_, "\U201C", simplify = TRUE, n = 2)[,1]) %>%
+        mutate(season_ = replace(season_, season_ == "", NA)) %>%
+        tidyr::fill(season_) %>%
+        mutate(season_short_ = as.numeric(stringr::str_split(season_, "-", simplify = TRUE, n = 2)[,1]) + 1) %>%
+        mutate(birthday = vitals[["birthday"]]) %>%
+        mutate(draft_eligibility_date_ = stringr::str_c(as.character(season_short_), "09-15", sep = "-")) %>%
+        mutate(age_ = elite::get_years_difference(birthday, draft_eligibility_date_)) %>%
+        mutate_all(stringr::str_squish) %>%
+        mutate_all(as.character) %>%      
+        mutate_all(~na_if(., "")) %>%
+        select(-c(blank_, playoffs_, draft_eligibility_date_, birthday)) %>%
+        select(team_, league_, captaincy_, season_, season_short_, age_, games_played_, goals_, assists_, points_, penalty_minutes_, plus_minus_, games_played_playoffs_, goals_playoffs_, assists_playoffs_, points_playoffs_, penalty_minutes_playoffs_, plus_minus_playoffs_) %>% 
+        mutate_at(vars(c(team_, league_, captaincy_, season_)), as.character) %>%
+        mutate_at(vars(-c(team_, league_, captaincy_, season_)), as.numeric) %>%
+        tidyr::nest()
+      
+      }
+      
+      all_data <- vitals %>% 
+        bind_cols(player_statistics) %>% 
+        rename(player_statistics = data)
+      
+    }
     
     if (.progress) {pb$tick()}
     
-    return(all_data)}
+    return(all_data)
+    
+  }
   
   player_stats_individual <- purrr::pmap_dfr(..., elite::persistently(.get_player_stats_individual, max_attempts = 10))
 
   mydata <- player_stats_individual %>% 
-    bind_cols(...) %>%
-    mutate(season_short = as.numeric(stringr::str_split(season, "-", simplify = TRUE, n = 2)[,1]) + 1) %>%
-    mutate(draft_eligibility_date = stringr::str_c(as.character(season_short), "09-15", sep = "-")) %>%
-    mutate(age = elite::get_years_difference(birthday, draft_eligibility_date)) %>%
-    select(name, team, league, position, shot_handedness, birth_place, birth_country, birthday, height, weight, season, season_short, age, games_played, goals, assists, points, penalty_minutes, plus_minus, games_played_playoffs, goals_playoffs, assists_playoffs, points_playoffs, penalty_minutes_playoffs, plus_minus_playoffs, player_url, team_url, name_, position_, player_url_, player_statistics) %>%
-    mutate_at(vars(c(name, team, league, position, shot_handedness, birth_place, birth_country, birthday, season, player_url, team_url, name_, position_, player_url_)), as.character) %>%
-    mutate_at(vars(-c(name, team, league, position, shot_handedness, birth_place, birth_country, birthday, season, player_url, team_url, name_, position_, player_url_, player_statistics)), as.numeric)
+    bind_cols(...)
+  
+  if (!c("pick_number" %in% colnames(mydata))) {
+  
+    mydata <- mydata %>%
+      mutate(season_short = as.numeric(stringr::str_split(season, "-", simplify = TRUE, n = 2)[,1]) + 1) %>%
+      mutate(draft_eligibility_date = stringr::str_c(as.character(season_short), "09-15", sep = "-")) %>%
+      mutate(age = elite::get_years_difference(birthday, draft_eligibility_date)) %>%
+      select(name, team, league, position, shot_handedness, birth_place, birth_country, birthday, height, weight, season, season_short, age, games_played, goals, assists, points, penalty_minutes, plus_minus, games_played_playoffs, goals_playoffs, assists_playoffs, points_playoffs, penalty_minutes_playoffs, plus_minus_playoffs, player_url, team_url, name_, position_, player_url_, player_statistics) %>%
+      mutate_at(vars(c(name, team, league, position, shot_handedness, birth_place, birth_country, birthday, season, player_url, team_url, name_, position_, player_url_)), as.character) %>%
+      mutate_at(vars(-c(name, team, league, position, shot_handedness, birth_place, birth_country, birthday, season, player_url, team_url, name_, position_, player_url_, player_statistics)), as.numeric)
+  
+  }
+  
+  else {
+    
+    mydata <- mydata %>%
+      mutate(draft_eligibility_date = stringr::str_c(draft_season, "09-15", sep = "-")) %>%
+      mutate(age = elite::get_years_difference(birthday, draft_eligibility_date)) %>%
+      select(draft_league, draft_season, pick_number, round, draft_team, name, position, shot_handedness, birth_place, birth_country, birthday, height, weight, age, player_url, name_, position_, player_url_, player_statistics) %>%
+      mutate_at(vars(c(draft_league, draft_season, draft_team, name, position, shot_handedness, birth_place, birth_country, birthday, name_, position_, player_url_, player_url)), as.character) %>%
+      mutate_at(vars(-c(draft_league, draft_season, draft_team, name, position, shot_handedness, birth_place, birth_country, birthday, name_, position_, player_url_, player_url, player_statistics)), as.numeric)
+    
+  }
   
   if (.strip_redundancy) {mydata <- mydata %>% select(-c(name_, position_, player_url_))}
   
